fd1dc123c6e58f435274c4c2086ed0aa46e730b1
8sosa <emmanuel.amos@stu.cu.edu.ng>
2025-03-11 11:53:58 +0100

    redddd

--- Middleware/redisConnect.js
+++ Middleware/redisConnect.js
@@ -1,15 +1,16 @@
-const Redis = require('ioredis');
+// const Redis = require('ioredis');
 
-if (!process.env.REDIS_URL) {
-  throw new Error("REDIS_URL environment variable is not set!");
-}
+// if (!process.env.REDIS_URL) {
+//   throw new Error("REDIS_URL environment variable is not set!");
+// }
 
-const redisClient = new Redis(process.env.REDIS_URL, {
-  tls: process.env.REDIS_URL.startsWith('rediss://') ? {} : undefined,
-});
+// const redisClient = new Redis(process.env.REDIS_URL, {
+//   tls: process.env.REDIS_URL.startsWith('rediss://') ? {} : undefined,
+// });
 
-redisClient.on('error', (err) => console.error('Redis error:', err));
-redisClient.on('connect', () => console.log('Connected to Redis!'));
-redisClient.on('ready', () => console.log('Redis client is ready.'));
+// redisClient.on('error', (err) => console.error('Redis error:', err));
+// redisClient.on('connect', () => console.log('Connected to Redis!'));
+// redisClient.on('ready', () => console.log('Redis client is ready.'));
 
-module.exports = redisClient;
+// module.exports = redisClient;
+// // 
--- Middleware/session.js
+++ Middleware/session.js
@@ -1,20 +1,20 @@
-const sessionMiddleware = (req, res, next) => {
-    // Ensure req.body exists
-    if (!req.body) {
-        req.body = {};
-    }
+// const sessionMiddleware = (req, res, next) => {
+//     // Ensure req.body exists
+//     if (!req.body) {
+//         req.body = {};
+//     }
 
-    const sessionId = req.sessionId;  // Extract sessionId from cookies
-    req.body.sessionId = sessionId;
+//     const sessionId = req.sessionId;  // Extract sessionId from cookies
+//     req.body.sessionId = sessionId;
 
-    if (!sessionId) {
-        return res.status(400).json({ message: 'Session ID is required' });
-    }
+//     if (!sessionId) {
+//         return res.status(400).json({ message: 'Session ID is required' });
+//     }
 
-    // Add sessionId to request object for easy access in controllers
-    req.sessionId = sessionId;
+//     // Add sessionId to request object for easy access in controllers
+//     req.sessionId = sessionId;
     
-    next();
-}
+//     next();
+// }
 
-module.exports = sessionMiddleware;
+// module.exports = sessionMiddleware;
