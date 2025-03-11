8f10be9fe88381b159edd5737de303b1ef9974ea
8sosa <emmanuel.amos@stu.cu.edu.ng>
2025-03-11 11:05:22 +0100

    vcf

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(sessionMiddleware);
 app.use(cors({
-    origin: 'https://she-unites-africa.onrender.com',
+    origin: 'https://violetcarefoundation.netlify.app',
     credentials: true,
 }));
 
