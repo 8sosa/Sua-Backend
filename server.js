4ee3ec020b68bdd1c305eff8440126db16f4a1f4
8sosa <emmanuel.amos@stu.cu.edu.ng>
2025-04-30 20:04:47 +0100

    add newly hosted origin

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.use(express.json());
 app.use(cookieParser());
 // app.use(sessionMiddleware);
 app.use(cors({
-    origin: 'https://violetcarefoundation.netlify.app',
+    origin: 'https://violetcarefoundation.com',
     credentials: true,
 }));
 
